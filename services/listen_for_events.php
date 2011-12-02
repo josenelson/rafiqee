5b6497b3cd9c71120e7785b39c68e272975fb148
Kartikeya <kartikeyadubey@gmail.com>
2011-12-01 19:05:35 -0800

    Listen to event and return data

--- services/listen_for_events.php
+++ services/listen_for_events.php
@@ -1,11 +1,12 @@
 <?php
 
-
+include_once("./config.php");
 require_once("./messaging.php");
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
+
 
 
 $canvas_id = isset($_GET["canvas_id"])? $_GET["canvas_id"]:die("canvas id not set");
@@ -25,8 +26,40 @@ $events = wait_for_events($canvas_id, $user_id, $timestamp);
 *	}
 *
 */
+$count = 0;
+foreach($events["events"] as $event)
+{
+	switch ($event['event_type'])
+	{
+	case 1: 
+	case 2: 
+	case 3: $events["events"][$count]["data"] = getElementData($event['source']);
+		break;
+	case 4:
+	case 5:
+	case 6: break;
+
+	}
+	$count++;
+}
 
 echo json_encode($events);
 
-
+function getElementData($elementid)
+{
+	$query = "SELECT * FROM elements WHERE id=".$elementid;
+	$result = mysql_query($query);
+	$retVal = array();
+	//If we get a result return it
+	if($result)
+	{
+		while($row = mysql_fetch_assoc($result))
+		{
+			$retVal["properties"] = $row["properties"];
+			$retVal["content"] = $row["content"];
+			$retVal["styles"] = $row["styles"];
+		}
+	}
+	return $retVal;
+}
 
