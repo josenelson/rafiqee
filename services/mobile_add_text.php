f3bdeb5f0f1a20778a82527a08a6cd10c835bb4e
Kartikeya <kartikeyadubey@gmail.com>
2011-12-02 00:55:15 -0800

    variable consistency

--- services/mobile_add_text.php
+++ services/mobile_add_text.php
@@ -4,13 +4,14 @@
  */
 include_once("./config.php");
 
-$userid = isset($_POST["userid"])? $_POST["userid"]:json_die("userid not set");
-$canvasid = isset($_POST["canvasid"])? $_POST["canvasid"]:json_die("canvasid not set");
+$user_id = isset($_POST["user_id"])? $_POST["user_id"]:json_die("user_id not set");
+$canvas_id = isset($_POST["canvas_id"])? $_POST["canvas_id"]:json_die("canvas_id not set");
 $content = isset($_POST["content"])? $_POST["content"]:json_die("text not set");
+$content = urldecode($content);
 
 $query = "INSERT INTO elements(user_id, canvas, content, styles) VALUES(";
-$query .= $userid.",";
-$query .= $canvasid.",'";
+$query .= $user_id.",";
+$query .= $canvas_id.",'";
 $query .= $content."','";
 $query .= "left: 50px, top: 50px'";
 $query .= ")";
