ad0aec5109d0313e75d639fec24f9d62b82d52ca
Kartikeya <kartikeyadubey@gmail.com>
2011-12-02 04:44:15 -0800

    colors for collabs and mobile image gets pushed into db

--- services/mobile_add_image.php
+++ services/mobile_add_image.php
@@ -6,6 +6,13 @@
  * add the link to the database
  */
 include_once("./config.php");
+include_once("./messaging.php");
+
+
+
+$user_id = isset($_POST["user_id"])? $_POST["user_id"]:json_die("user_id not set");
+$canvas_id = isset($_POST["canvas_id"])? $_POST["canvas_id"]:json_die("canvas_id not set");
+
 //First store image
 $base = $_REQUEST['image'];
 
@@ -16,3 +23,25 @@ $fileName = tempnam("./canvas-images/", "mobimg");
 $file = fopen("./canvas-images/".$fileName, 'wb');
 fwrite($file, $binary);
 fclose($file);
+
+
+
+$content = "<img src ="."./canvas-images/".$filename;
+$query = "INSERT INTO elements(user_id, canvas, content, styles) VALUES(";
+$query .= $user_id.",";
+$query .= $canvas_id.",'";
+$query .= $content."','";
+$query .= "left: 50px, top: 50px'";
+$query .= ")";
+$result = mysql_query($query);
+$source = mysql_insert_id();
+if($result)
+{
+	echo json_encode("success");
+	dispatch_event(5, $source, $canvas_id, $user_id);
+}
+else
+{
+	echo json_encode("failure");
+}
+
