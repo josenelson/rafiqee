6a0927d95e7501218771c5bb68bff797f29193ee
Kartikeya <kartikeyadubey@gmail.com>
2011-12-01 20:01:24 -0800

    updated listen_events so it returns data in cases

--- services/listen_for_events.php
+++ services/listen_for_events.php
@@ -33,11 +33,11 @@ foreach($events["events"] as $event)
 	{
 	case 1: 
 	case 2: 
-	case 3: $events["events"][$count]["data"] = getElementData($event['source']);
-		break;
+	case 3: 
 	case 4:
-	case 5:
-	case 6: break;
+	case 5: 
+	case 6: $events["events"][$count]["data"] = getElementData($event['source']);
+		break;
 
 	}
 	$count++;
